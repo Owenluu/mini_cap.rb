180517c1e2081719287c44eb54dcbbfb68115790
Owen Luu <owenluu111@gmail.com>
2021-01-23 15:44:53 -0600

    created all routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "product#update"
+    delete "/products/:id" => "product#destroy"
   end
 end
