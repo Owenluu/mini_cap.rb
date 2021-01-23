5abda082484d387342ecfd0fa7b9ddab69d9c4ab
Owen Luu <owenluu111@gmail.com>
2021-01-23 15:48:39 -0600

    added the other instances for product controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,20 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "index.json.jb"
   end
+
+  def show
+    render "show.json.jb"
+  end
+
+  def create
+    render "show.json.jb"
+  end
+
+  def update
+    render "show.json.jb"
+  end
+
+  def destroy
+    render json: { message: "Products Sucessfully Destroyed!" }
+  end
 end
