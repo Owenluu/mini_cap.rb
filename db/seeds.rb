c76baea1c61717f2f53899f37401a585e3de8961
Owen Luu <owenluu111@gmail.com>
2021-01-23 15:01:19 -0600

    fixed typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,44 @@
-product = Product.new({name: "Air Jordan 1 Chicago" price: 1900 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Air Jordan 1 Chicago", price: 1900, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Air Jordan 4 Breds/ Black Cement" price: 475 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Air Jordan 4 Breds/ Black Cement", price: 475, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Air Jordan 11 Breds/ Playoffs" price: description: 373 "Deadstock: Size 10" })
+product = Product.new({ name: "Air Jordan 11 Breds/ Playoffs", price: 373, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Air Jordan 11 Concord" price: 400 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Air Jordan 11 Concord", price: 400, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Air Jordan 1 Retro High Off-White Chicago " price: 5000 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Air Jordan 1 Retro High Off-White Chicago ", price: 5000, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Nike Tailwind 79 Stranger Things" price: 244 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Nike Tailwind 79 Stranger Things", price: 244, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Nike React Element 87 Royal TInt" price: 174 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Nike React Element 87 Royal Tint", price: 174, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Nike Air Max 97 Shanghai" price: 500 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Nike Air Max 97 Shanghai", price: 500, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Nike SB Dunk Low J-Pack Chicago" price: 530 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Nike SB Dunk Low J-Pack Chicago", price: 530, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "Nike Air Force 1 Low White '07" price: 90 description: "Deadstock: Size 10" })
+product = Product.new({ name: "Nike Air Force 1 Low White '07", price: 90, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "adidas NMD HU Pharrell Inspiration Pack Clear Sky" price: 275 description: "Deadstock: Size 10" })
+product = Product.new({ name: "adidas NMD HU Pharrell Inspiration Pack Clear Sky", price: 275, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "adidas Ultra Boost 4.0 Game of Thrones White Walkers" price: 265 description: "Deadstock: Size 10" })
+product = Product.new({ name: "adidas Ultra Boost 4.0 Game of Thrones White Walkers", price: 265, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "adidas Yeezy Boost 350 V@ Zebra" price: 325 description: "Deadstock: Size 10" })
+product = Product.new({ name: "adidas Yeezy Boost 350 V@ Zebra", price: 325, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "adidas Stan Smith White Green (OG)" price: 50 description: "Deadstock: Size 10" })
+product = Product.new({ name: "adidas Stan Smith White Green (OG)", price: 50, description: "Deadstock: Size 10" })
 product.save
 
-product = Product.new({name: "adidas Nite Jogger White Shock Red" price: 60 description: "Deadstock: Size 10" })
+product = Product.new({ name: "adidas Nite Jogger White Shock Red", price: 60, description: "Deadstock: Size 10" })
 product.save
